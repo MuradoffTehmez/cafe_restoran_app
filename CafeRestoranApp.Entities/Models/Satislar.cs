b0f662ca34411f21deec4757301cb5046f7f94b8
MuradoffTehmez <muradofftehmez01@gmail.com>
2024-11-28 17:21:45 +0400

    satislar

---
+++ CafeRestoranApp.Entities/Models/Satislar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeRestoranApp.Entities.Models
+{
+    [Table("Satislar")]
+    public class Satislar
+    {
+      
+        public int Id { get; set; }
+
+        public string SatisKodu { get; set; } 
+
+        public decimal Tutar { get; set; }
+
+        public decimal Odenis { get; set; }
+
+        public decimal Qalan { get; set; }
+
+
+
+    }
+}
--- CafeRestoranApp.Entities/Models/masalar.cs
+++ CafeRestoranApp.Entities/Models/masalar.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CafeRestoranApp.Entities.Models
 {
+    [Table("Masalar")]
     public class masalar
     {
-        public int id { get; set; }
+        public int Id { get; set; }
+
+        public string MasaAdi { get; set; }
+
+        public string Aciklama { get; set; }
+
+        public bool Durumu {  get; set; }   
+
+        public bool Rezervasiya {  get; set; }
+
+        public DateTime ElaveOlmaTarixi { get; set; }
+
+        public DateTime SonIslemTarixi { get; set; }
+
 
     }
 }
