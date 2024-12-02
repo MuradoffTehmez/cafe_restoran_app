ba21ba3b6eb9ea86171c2120c090ac1a54ac9544
MuradoffTehmez <muradofftehmez01@gmail.com>
2024-12-02 23:40:16 +0400

    Musteriler cs yaratildi

---
+++ CafeRestoranApp.Entities/Mapping/MusterilerMap.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CafeRestoranApp.Entities.Interfaces;
+using CafeRestoranApp.Entities.Models;
+
+namespace CafeRestoranApp.Entities.Mapping
+{
+    public class MusterilerMap : EntityTypeConfiguration<Musteriler>
+    {
+        public MusterilerMap()
+        {
+            this.ToTable("Musteriler");
+
+            this.HasKey(p => p.Id);
+
+            this.Property(p => p.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            this.Property(p => p.AdSoyad)
+                .HasColumnType("varchar")
+                .HasMaxLength(150);
+
+            this.Property(p => p.Telefon)
+                .HasColumnType("varchar")
+                .HasMaxLength(25);
+
+            this.Property(p => p.Adres)
+                .HasColumnType("varchar")
+                .HasMaxLength(200);
+
+            this.Property(p => p.Email)
+                .HasColumnType("varchar")
+                .HasMaxLength(150);
+
+            this.Property(p => p.Aciklama)
+                .HasColumnType("varchar")
+                .HasMaxLength(500);
+        }
+    }
+}
---
+++ CafeRestoranApp.Entities/Models/Musteriler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CafeRestoranApp.Entities.Interfaces;
+
+namespace CafeRestoranApp.Entities.Models
+{
+    public class Musteriler : IEntity
+    {
+        public int Id { get; set; }
+
+        public string AdSoyad { get; set; }
+
+        public string Telefon { get; set; }
+
+        public string Adres { get; set; }
+
+        public string Email { get; set; }
+
+        public string Aciklama { get; set; }
+
+        public DateTime Tarix { get; set; }
+
+    }
+}
