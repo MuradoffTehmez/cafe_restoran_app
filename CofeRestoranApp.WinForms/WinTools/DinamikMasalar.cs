757cc22eb745d973fcbdede45c3d0c505fc20c84
MuradoffTehmez <muradofftehmez01@gmail.com>
2024-12-02 17:41:00 +0400

    FlowLayoutPanel

--- CofeRestoranApp.WinForms/WinTools/DinamikMasalar.cs
+++ CofeRestoranApp.WinForms/WinTools/DinamikMasalar.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using CafeRestoranApp.Entities.Models;
 
 namespace CofeRestoranApp.WinForms.WinTools
 {
     public class DinamikMasalar
     {
-        public void MasaGetir()
+        public void MasaGetir(CafeContext context,FlowLayoutPanel pnl)
         {
 
         }
