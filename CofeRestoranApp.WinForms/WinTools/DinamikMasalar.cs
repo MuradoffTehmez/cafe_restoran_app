33aff4986f970a2df994d9484833b6aaa1a361ce
MuradoffTehmez <muradofftehmez01@gmail.com>
2024-12-02 17:46:36 +0400

    var btn = new CheckButton(); btn.Text = model[i].MasaAdi;

--- CofeRestoranApp.WinForms/WinTools/DinamikMasalar.cs
+++ CofeRestoranApp.WinForms/WinTools/DinamikMasalar.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CafeRestoranApp.Entities.Models;
+using DevExpress.XtraBars.Customization.Helpers;
 
 namespace CofeRestoranApp.WinForms.WinTools
 {
@@ -12,7 +13,12 @@ namespace CofeRestoranApp.WinForms.WinTools
     {
         public void MasaGetir(CafeContext context,FlowLayoutPanel pnl)
         {
-
+            var model = context.Masalar.ToList();
+            for (int i = 0; i < model.Count; i++)
+            {
+                var btn = new CheckButton();
+                btn.Text = model[i].MasaAdi;
+            }
         }
     }
 }
