dfc88c880964231e0626ee16f1712ce59305b914
MuradoffTehmez <muradofftehmez01@gmail.com>
2024-12-02 20:56:19 +0400

    maslar durumunu update etdim

--- CofeRestoranApp.WinForms/WinTools/DinamikMasalar.cs
+++ CofeRestoranApp.WinForms/WinTools/DinamikMasalar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace CofeRestoranApp.WinForms.WinTools
 {
     public class DinamikMasalar
     {
-        public static void MasaGetir(CafeContext context,FlowLayoutPanel pnl)
+        public static void MasaGetir(CafeContext context, FlowLayoutPanel pnl)
         {
             var model = context.Masalar.ToList();
             for (int i = 0; i < model.Count; i++)
@@ -19,18 +20,31 @@ namespace CofeRestoranApp.WinForms.WinTools
                 var btn = new CheckButton();
                 btn.Text = model[i].MasaAdi;
                 btn.Name = model[i].Id.ToString();
-                btn.Height = 100;
-                btn.Width = 80;
+                btn.Height = 150;
+                btn.Width = 150;
                 pnl.Controls.Add(btn);
-                //btn.Click += Btn_Click;
+                if (model[i].Rezervasiya && !model[i].Durumu)
+                {
+                    btn.Appearance.BackColor = Color.Green;
+                }
+                else if (model[i].Durumu)
+                {
+                    btn.Appearance.BackColor = Color.Red;
+                }
+                else if (!model[i].Durumu)
+                {
+                    btn.Appearance.BackColor = Color.Blue;
+                }
+                btn.Click += Btn_Click;
 
             }
         }
 
-        private void Btn_Click(object sender, EventArgs e)
+        private static void Btn_Click(object sender, EventArgs e)
         {
             var btn = sender as CheckButton;
-            MessageBox.Show(btn.Text);
+            MessageBox.Show($"Masa Adi: {btn.Text} Masa ID: {btn.Name}");
+
         }
     }
 }
