4a694f8162e17d9344029b3b048cb4b7bd82c8f2
MuradoffTehmez <muradofftehmez01@gmail.com>
2024-12-02 19:29:00 +0400

    form ayarlari ve dizayin

--- CofeRestoranApp.WinForms/Masalar/Frm_Masa_Durumu.Designer.cs
+++ CofeRestoranApp.WinForms/Masalar/Frm_Masa_Durumu.Designer.cs
@@ -32,8 +32,7 @@
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.Group_Emeliyyatlar = new DevExpress.XtraEditors.GroupControl();
             this.btn_Guncelle = new DevExpress.XtraEditors.SimpleButton();
-            this.Btn_Duzenle = new DevExpress.XtraEditors.SimpleButton();
-            this.Btn_Yeni = new DevExpress.XtraEditors.SimpleButton();
+            this.Btn_Yeni_Sifaris = new DevExpress.XtraEditors.SimpleButton();
             this.btn_cisix_et = new DevExpress.XtraEditors.SimpleButton();
             this.Btn_Kayd_Et = new DevExpress.XtraEditors.SimpleButton();
             this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
@@ -41,6 +40,12 @@
             this.gridControl1 = new DevExpress.XtraGrid.GridControl();
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.Group_Hesablamalar = new DevExpress.XtraEditors.GroupControl();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.Btn_Dolu_Masa = new DevExpress.XtraEditors.SimpleButton();
+            this.Btn_Bos_Masa = new DevExpress.XtraEditors.SimpleButton();
+            this.Btn_Rezervli_Masa = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.calcEdit1 = new DevExpress.XtraEditors.CalcEdit();
             ((System.ComponentModel.ISupportInitialize)(this.Group_Emeliyyatlar)).BeginInit();
             this.Group_Emeliyyatlar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
@@ -52,12 +57,18 @@
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Group_Hesablamalar)).BeginInit();
+            this.Group_Hesablamalar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.calcEdit1.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // labelControl1
             // 
             this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.ForeColor = System.Drawing.Color.Red;
             this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Appearance.Options.UseForeColor = true;
             this.labelControl1.Appearance.Options.UseTextOptions = true;
             this.labelControl1.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
             this.labelControl1.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
@@ -67,19 +78,18 @@
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(1596, 43);
             this.labelControl1.TabIndex = 3;
-            this.labelControl1.Text = "Masalar";
+            this.labelControl1.Text = "Masalar Haqqinda Melumat";
             // 
             // Group_Emeliyyatlar
             // 
             this.Group_Emeliyyatlar.Controls.Add(this.btn_Guncelle);
-            this.Group_Emeliyyatlar.Controls.Add(this.Btn_Duzenle);
-            this.Group_Emeliyyatlar.Controls.Add(this.Btn_Yeni);
+            this.Group_Emeliyyatlar.Controls.Add(this.Btn_Yeni_Sifaris);
             this.Group_Emeliyyatlar.Controls.Add(this.btn_cisix_et);
             this.Group_Emeliyyatlar.Controls.Add(this.Btn_Kayd_Et);
             this.Group_Emeliyyatlar.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.Group_Emeliyyatlar.Location = new System.Drawing.Point(0, 719);
+            this.Group_Emeliyyatlar.Location = new System.Drawing.Point(0, 749);
             this.Group_Emeliyyatlar.Name = "Group_Emeliyyatlar";
-            this.Group_Emeliyyatlar.Size = new System.Drawing.Size(1596, 111);
+            this.Group_Emeliyyatlar.Size = new System.Drawing.Size(1596, 81);
             this.Group_Emeliyyatlar.TabIndex = 4;
             this.Group_Emeliyyatlar.Text = "Əməliyyatlar";
             // 
@@ -90,37 +100,24 @@
             this.btn_Guncelle.Appearance.Options.UseTextOptions = true;
             this.btn_Guncelle.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
             this.btn_Guncelle.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Guncelle.ImageOptions.SvgImage")));
-            this.btn_Guncelle.Location = new System.Drawing.Point(372, 34);
+            this.btn_Guncelle.Location = new System.Drawing.Point(144, 26);
             this.btn_Guncelle.Name = "btn_Guncelle";
-            this.btn_Guncelle.Size = new System.Drawing.Size(125, 60);
+            this.btn_Guncelle.Size = new System.Drawing.Size(125, 46);
             this.btn_Guncelle.TabIndex = 4;
             this.btn_Guncelle.Text = "Güncəllə";
             // 
-            // Btn_Duzenle
+            // Btn_Yeni_Sifaris
             // 
-            this.Btn_Duzenle.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_Duzenle.Appearance.Options.UseFont = true;
-            this.Btn_Duzenle.Appearance.Options.UseTextOptions = true;
-            this.Btn_Duzenle.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-            this.Btn_Duzenle.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Duzenle.ImageOptions.SvgImage")));
-            this.Btn_Duzenle.Location = new System.Drawing.Point(192, 34);
-            this.Btn_Duzenle.Name = "Btn_Duzenle";
-            this.Btn_Duzenle.Size = new System.Drawing.Size(125, 60);
-            this.Btn_Duzenle.TabIndex = 2;
-            this.Btn_Duzenle.Text = "Dəyişdir";
-            // 
-            // Btn_Yeni
-            // 
-            this.Btn_Yeni.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_Yeni.Appearance.Options.UseFont = true;
-            this.Btn_Yeni.Appearance.Options.UseTextOptions = true;
-            this.Btn_Yeni.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-            this.Btn_Yeni.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("brn_Elave_Et.ImageOptions.SvgImage")));
-            this.Btn_Yeni.Location = new System.Drawing.Point(12, 34);
-            this.Btn_Yeni.Name = "Btn_Yeni";
-            this.Btn_Yeni.Size = new System.Drawing.Size(125, 60);
-            this.Btn_Yeni.TabIndex = 0;
-            this.Btn_Yeni.Text = "Əlavə et";
+            this.Btn_Yeni_Sifaris.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Yeni_Sifaris.Appearance.Options.UseFont = true;
+            this.Btn_Yeni_Sifaris.Appearance.Options.UseTextOptions = true;
+            this.Btn_Yeni_Sifaris.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.Btn_Yeni_Sifaris.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("brn_Elave_Et.ImageOptions.SvgImage")));
+            this.Btn_Yeni_Sifaris.Location = new System.Drawing.Point(13, 26);
+            this.Btn_Yeni_Sifaris.Name = "Btn_Yeni_Sifaris";
+            this.Btn_Yeni_Sifaris.Size = new System.Drawing.Size(125, 46);
+            this.Btn_Yeni_Sifaris.TabIndex = 0;
+            this.Btn_Yeni_Sifaris.Text = "Sifaris \r\nelave et";
             // 
             // btn_cisix_et
             // 
@@ -130,9 +127,9 @@
             this.btn_cisix_et.Appearance.Options.UseTextOptions = true;
             this.btn_cisix_et.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
             this.btn_cisix_et.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_cisix_et.ImageOptions.SvgImage")));
-            this.btn_cisix_et.Location = new System.Drawing.Point(1466, 34);
+            this.btn_cisix_et.Location = new System.Drawing.Point(1467, 26);
             this.btn_cisix_et.Name = "btn_cisix_et";
-            this.btn_cisix_et.Size = new System.Drawing.Size(125, 60);
+            this.btn_cisix_et.Size = new System.Drawing.Size(125, 46);
             this.btn_cisix_et.TabIndex = 3;
             this.btn_cisix_et.Text = "Çıxış";
             // 
@@ -144,9 +141,9 @@
             this.Btn_Kayd_Et.Appearance.Options.UseTextOptions = true;
             this.Btn_Kayd_Et.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
             this.Btn_Kayd_Et.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btn_Sil.ImageOptions.Image")));
-            this.Btn_Kayd_Et.Location = new System.Drawing.Point(1335, 34);
+            this.Btn_Kayd_Et.Location = new System.Drawing.Point(1336, 26);
             this.Btn_Kayd_Et.Name = "Btn_Kayd_Et";
-            this.Btn_Kayd_Et.Size = new System.Drawing.Size(125, 60);
+            this.Btn_Kayd_Et.Size = new System.Drawing.Size(125, 46);
             this.Btn_Kayd_Et.TabIndex = 1;
             this.Btn_Kayd_Et.Text = "Qeyd et";
             // 
@@ -160,6 +157,7 @@
             // splitContainerControl1.Panel1
             // 
             this.splitContainerControl1.Panel1.Controls.Add(this.flowLayoutPanel1);
+            this.splitContainerControl1.Panel1.Controls.Add(this.panelControl1);
             this.splitContainerControl1.Panel1.Text = "Panel1";
             // 
             // splitContainerControl1.Panel2
@@ -167,8 +165,8 @@
             this.splitContainerControl1.Panel2.Controls.Add(this.gridControl1);
             this.splitContainerControl1.Panel2.Controls.Add(this.Group_Hesablamalar);
             this.splitContainerControl1.Panel2.Text = "Panel2";
-            this.splitContainerControl1.Size = new System.Drawing.Size(1596, 676);
-            this.splitContainerControl1.SplitterPosition = 272;
+            this.splitContainerControl1.Size = new System.Drawing.Size(1596, 706);
+            this.splitContainerControl1.SplitterPosition = 319;
             this.splitContainerControl1.TabIndex = 5;
             // 
             // flowLayoutPanel1
@@ -177,7 +175,7 @@
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1596, 272);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1596, 271);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // gridControl1
@@ -186,7 +184,7 @@
             this.gridControl1.Location = new System.Drawing.Point(0, 0);
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(1596, 270);
+            this.gridControl1.Size = new System.Drawing.Size(1596, 293);
             this.gridControl1.TabIndex = 1;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -198,12 +196,96 @@
             // 
             // Group_Hesablamalar
             // 
+            this.Group_Hesablamalar.Controls.Add(this.calcEdit1);
+            this.Group_Hesablamalar.Controls.Add(this.labelControl2);
             this.Group_Hesablamalar.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.Group_Hesablamalar.Location = new System.Drawing.Point(0, 270);
+            this.Group_Hesablamalar.Location = new System.Drawing.Point(0, 293);
             this.Group_Hesablamalar.Name = "Group_Hesablamalar";
-            this.Group_Hesablamalar.Size = new System.Drawing.Size(1596, 124);
+            this.Group_Hesablamalar.Size = new System.Drawing.Size(1596, 84);
             this.Group_Hesablamalar.TabIndex = 0;
             this.Group_Hesablamalar.Text = "Hesablamalar";
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.Btn_Rezervli_Masa);
+            this.panelControl1.Controls.Add(this.Btn_Bos_Masa);
+            this.panelControl1.Controls.Add(this.Btn_Dolu_Masa);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControl1.Location = new System.Drawing.Point(0, 271);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(1596, 48);
+            this.panelControl1.TabIndex = 1;
+            // 
+            // Btn_Dolu_Masa
+            // 
+            this.Btn_Dolu_Masa.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Question;
+            this.Btn_Dolu_Masa.Appearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.Btn_Dolu_Masa.Appearance.Font = new System.Drawing.Font("Tahoma", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Dolu_Masa.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.Btn_Dolu_Masa.Appearance.Options.UseBackColor = true;
+            this.Btn_Dolu_Masa.Appearance.Options.UseBorderColor = true;
+            this.Btn_Dolu_Masa.Appearance.Options.UseFont = true;
+            this.Btn_Dolu_Masa.Appearance.Options.UseForeColor = true;
+            this.Btn_Dolu_Masa.Location = new System.Drawing.Point(12, 6);
+            this.Btn_Dolu_Masa.Name = "Btn_Dolu_Masa";
+            this.Btn_Dolu_Masa.Size = new System.Drawing.Size(180, 37);
+            this.Btn_Dolu_Masa.TabIndex = 0;
+            this.Btn_Dolu_Masa.Text = "Dolu Masa";
+            // 
+            // Btn_Bos_Masa
+            // 
+            this.Btn_Bos_Masa.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Danger;
+            this.Btn_Bos_Masa.Appearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.Btn_Bos_Masa.Appearance.Font = new System.Drawing.Font("Tahoma", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Bos_Masa.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.Btn_Bos_Masa.Appearance.Options.UseBackColor = true;
+            this.Btn_Bos_Masa.Appearance.Options.UseBorderColor = true;
+            this.Btn_Bos_Masa.Appearance.Options.UseFont = true;
+            this.Btn_Bos_Masa.Appearance.Options.UseForeColor = true;
+            this.Btn_Bos_Masa.Location = new System.Drawing.Point(198, 5);
+            this.Btn_Bos_Masa.Name = "Btn_Bos_Masa";
+            this.Btn_Bos_Masa.Size = new System.Drawing.Size(180, 37);
+            this.Btn_Bos_Masa.TabIndex = 1;
+            this.Btn_Bos_Masa.Text = "Bos Masa";
+            // 
+            // Btn_Rezervli_Masa
+            // 
+            this.Btn_Rezervli_Masa.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Success;
+            this.Btn_Rezervli_Masa.Appearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.Btn_Rezervli_Masa.Appearance.Font = new System.Drawing.Font("Tahoma", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Rezervli_Masa.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.Btn_Rezervli_Masa.Appearance.Options.UseBackColor = true;
+            this.Btn_Rezervli_Masa.Appearance.Options.UseBorderColor = true;
+            this.Btn_Rezervli_Masa.Appearance.Options.UseFont = true;
+            this.Btn_Rezervli_Masa.Appearance.Options.UseForeColor = true;
+            this.Btn_Rezervli_Masa.Location = new System.Drawing.Point(384, 5);
+            this.Btn_Rezervli_Masa.Name = "Btn_Rezervli_Masa";
+            this.Btn_Rezervli_Masa.Size = new System.Drawing.Size(180, 37);
+            this.Btn_Rezervli_Masa.TabIndex = 2;
+            this.Btn_Rezervli_Masa.Text = "Rezervasiya";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.BorderColor = System.Drawing.Color.Silver;
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseBorderColor = true;
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Appearance.Options.UseTextOptions = true;
+            this.labelControl2.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
+            this.labelControl2.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl2.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.labelControl2.Location = new System.Drawing.Point(12, 38);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(92, 22);
+            this.labelControl2.TabIndex = 0;
+            this.labelControl2.Text = "Odenis :";
+            // 
+            // calcEdit1
+            // 
+            this.calcEdit1.Location = new System.Drawing.Point(110, 40);
+            this.calcEdit1.Name = "calcEdit1";
+            this.calcEdit1.Size = new System.Drawing.Size(144, 20);
+            this.calcEdit1.TabIndex = 0;
             // 
             // Frm_Masa_Durumu
             // 
@@ -229,6 +311,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Group_Hesablamalar)).EndInit();
+            this.Group_Hesablamalar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.calcEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -238,8 +324,7 @@
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.GroupControl Group_Emeliyyatlar;
         private DevExpress.XtraEditors.SimpleButton btn_Guncelle;
-        private DevExpress.XtraEditors.SimpleButton Btn_Duzenle;
-        private DevExpress.XtraEditors.SimpleButton Btn_Yeni;
+        private DevExpress.XtraEditors.SimpleButton Btn_Yeni_Sifaris;
         private DevExpress.XtraEditors.SimpleButton btn_cisix_et;
         private DevExpress.XtraEditors.SimpleButton Btn_Kayd_Et;
         private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
@@ -247,5 +332,11 @@
         private DevExpress.XtraEditors.GroupControl Group_Hesablamalar;
         private DevExpress.XtraGrid.GridControl gridControl1;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.SimpleButton Btn_Dolu_Masa;
+        private DevExpress.XtraEditors.SimpleButton Btn_Rezervli_Masa;
+        private DevExpress.XtraEditors.SimpleButton Btn_Bos_Masa;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.CalcEdit calcEdit1;
     }
 }
