2c91ba58f447faed39d298a9365d0c3740186e68
MuradoffTehmez <muradofftehmez01@gmail.com>
2024-12-03 02:32:03 +0400

    Data

--- CofeRestoranApp.WinForms/Musteriler/Frm_Musteriler.Designer.cs
+++ CofeRestoranApp.WinForms/Musteriler/Frm_Musteriler.Designer.cs
@@ -28,19 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Musteriler));
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.btn_cisix_et = new DevExpress.XtraEditors.SimpleButton();
+            this.btn_Sil = new DevExpress.XtraEditors.SimpleButton();
+            this.brn_Elave_et = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.musterilerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.colId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAdSoyad = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTelefon = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAdres = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colEmail = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAciklama = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTarix = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSatislar = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musterilerBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.btn_cisix_et);
+            this.groupControl1.Controls.Add(this.btn_Sil);
+            this.groupControl1.Controls.Add(this.brn_Elave_et);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupControl1.Location = new System.Drawing.Point(0, 485);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(1099, 80);
+            this.groupControl1.TabIndex = 4;
+            this.groupControl1.Text = "Əməliyyatlar";
+            // 
+            // btn_cisix_et
+            // 
+            this.btn_cisix_et.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_cisix_et.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cisix_et.Appearance.Options.UseFont = true;
+            this.btn_cisix_et.Appearance.Options.UseTextOptions = true;
+            this.btn_cisix_et.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.btn_cisix_et.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_cisix_et.ImageOptions.SvgImage")));
+            this.btn_cisix_et.Location = new System.Drawing.Point(938, 26);
+            this.btn_cisix_et.Name = "btn_cisix_et";
+            this.btn_cisix_et.Size = new System.Drawing.Size(149, 39);
+            this.btn_cisix_et.TabIndex = 3;
+            this.btn_cisix_et.Text = "Çıxış";
+            // 
+            // btn_Sil
+            // 
+            this.btn_Sil.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Sil.Appearance.Options.UseFont = true;
+            this.btn_Sil.Appearance.Options.UseTextOptions = true;
+            this.btn_Sil.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.btn_Sil.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btn_Sil.ImageOptions.Image")));
+            this.btn_Sil.Location = new System.Drawing.Point(179, 26);
+            this.btn_Sil.Name = "btn_Sil";
+            this.btn_Sil.Size = new System.Drawing.Size(149, 39);
+            this.btn_Sil.TabIndex = 1;
+            this.btn_Sil.Text = "Sil";
+            // 
+            // brn_Elave_et
+            // 
+            this.brn_Elave_et.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brn_Elave_et.Appearance.Options.UseFont = true;
+            this.brn_Elave_et.Appearance.Options.UseTextOptions = true;
+            this.brn_Elave_et.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.brn_Elave_et.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("brn_Elave_et.ImageOptions.Image")));
+            this.brn_Elave_et.Location = new System.Drawing.Point(12, 26);
+            this.brn_Elave_et.Name = "brn_Elave_et";
+            this.brn_Elave_et.Size = new System.Drawing.Size(149, 39);
+            this.brn_Elave_et.TabIndex = 0;
+            this.brn_Elave_et.Text = "Qeyd Et";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Appearance.Options.UseTextOptions = true;
+            this.labelControl1.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.labelControl1.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
+            this.labelControl1.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelControl1.Location = new System.Drawing.Point(0, 0);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(1099, 43);
+            this.labelControl1.TabIndex = 3;
+            this.labelControl1.Text = "Musteriler";
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.musterilerBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 43);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(1099, 442);
+            this.gridControl1.TabIndex = 5;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colId,
+            this.colAdSoyad,
+            this.colTelefon,
+            this.colAdres,
+            this.colEmail,
+            this.colAciklama,
+            this.colTarix,
+            this.colSatislar});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // musterilerBindingSource
+            // 
+            this.musterilerBindingSource.DataSource = typeof(CafeRestoranApp.Entities.Models.Musteriler);
+            // 
+            // colId
+            // 
+            this.colId.FieldName = "Id";
+            this.colId.Name = "colId";
+            this.colId.Visible = true;
+            this.colId.VisibleIndex = 0;
+            // 
+            // colAdSoyad
+            // 
+            this.colAdSoyad.FieldName = "AdSoyad";
+            this.colAdSoyad.Name = "colAdSoyad";
+            this.colAdSoyad.Visible = true;
+            this.colAdSoyad.VisibleIndex = 1;
+            // 
+            // colTelefon
+            // 
+            this.colTelefon.FieldName = "Telefon";
+            this.colTelefon.Name = "colTelefon";
+            this.colTelefon.Visible = true;
+            this.colTelefon.VisibleIndex = 2;
+            // 
+            // colAdres
+            // 
+            this.colAdres.FieldName = "Adres";
+            this.colAdres.Name = "colAdres";
+            this.colAdres.Visible = true;
+            this.colAdres.VisibleIndex = 3;
+            // 
+            // colEmail
+            // 
+            this.colEmail.FieldName = "Email";
+            this.colEmail.Name = "colEmail";
+            this.colEmail.Visible = true;
+            this.colEmail.VisibleIndex = 4;
+            // 
+            // colAciklama
+            // 
+            this.colAciklama.FieldName = "Aciklama";
+            this.colAciklama.Name = "colAciklama";
+            this.colAciklama.Visible = true;
+            this.colAciklama.VisibleIndex = 5;
+            // 
+            // colTarix
+            // 
+            this.colTarix.FieldName = "Tarix";
+            this.colTarix.Name = "colTarix";
+            this.colTarix.Visible = true;
+            this.colTarix.VisibleIndex = 6;
+            // 
+            // colSatislar
+            // 
+            this.colSatislar.FieldName = "Satislar";
+            this.colSatislar.Name = "colSatislar";
+            this.colSatislar.Visible = true;
+            this.colSatislar.VisibleIndex = 7;
             // 
             // Frm_Musteriler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1226, 653);
+            this.ClientSize = new System.Drawing.Size(1099, 565);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.groupControl1);
+            this.Controls.Add(this.labelControl1);
             this.Name = "Frm_Musteriler";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Frm_Musteriler";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musterilerBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.SimpleButton btn_cisix_et;
+        private DevExpress.XtraEditors.SimpleButton btn_Sil;
+        private DevExpress.XtraEditors.SimpleButton brn_Elave_et;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.BindingSource musterilerBindingSource;
+        private DevExpress.XtraGrid.Columns.GridColumn colId;
+        private DevExpress.XtraGrid.Columns.GridColumn colAdSoyad;
+        private DevExpress.XtraGrid.Columns.GridColumn colTelefon;
+        private DevExpress.XtraGrid.Columns.GridColumn colAdres;
+        private DevExpress.XtraGrid.Columns.GridColumn colEmail;
+        private DevExpress.XtraGrid.Columns.GridColumn colAciklama;
+        private DevExpress.XtraGrid.Columns.GridColumn colTarix;
+        private DevExpress.XtraGrid.Columns.GridColumn colSatislar;
     }
 }
