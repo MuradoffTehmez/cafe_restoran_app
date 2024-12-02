fec2e9f6e898dff6cac2a982e75f52f0d0f22a80
MuradoffTehmez <muradofftehmez01@gmail.com>
2024-12-02 23:49:24 +0400

    Validator

--- CafeRestoranApp.Entities/Validations/MusterilerVALIDATOR.cs
+++ CafeRestoranApp.Entities/Validations/MusterilerVALIDATOR.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CafeRestoranApp.Entities.Models;
+using FluentValidation;
 
 namespace CafeRestoranApp.Entities.Validations
 {
-    internal class MusterilerVALIDATOR
+    public class MusterilerVALIDATOR : AbstractValidator<Musteriler>
     {
+        public MusterilerVALIDATOR()
+        {
+            RuleFor(x => x.AdSoyad)
+                .NotEmpty().WithMessage("Ad Soyad  boş ola bilməz.");
 
+            RuleFor(x => x.Telefon)
+                .NotEmpty().WithMessage("Telefon Xanisi boş ola bilməz.");
+        }
     }
 }
