93df99e0eacc42ebc6a29664f3e48c3b067d580b
MuradoffTehmez <muradofftehmez01@gmail.com>
2024-12-04 03:54:43 +0400

    Masa revervasiyasi formunun dizayi hazirlandi

--- CofeRestoranApp.WinForms/Masalar/Frm_Masa_Rezervasiya.Designer.cs
+++ CofeRestoranApp.WinForms/Masalar/Frm_Masa_Rezervasiya.Designer.cs
@@ -28,7 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Masa_Rezervasiya));
             this.Lbn_Basliq = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl8 = new DevExpress.XtraEditors.LabelControl();
+            this.txtR_Proses = new DevExpress.XtraEditors.MemoEdit();
+            this.labelControl10 = new DevExpress.XtraEditors.LabelControl();
+            this.dateTarix = new DevExpress.XtraEditors.DateEdit();
+            this.Group_Emeliyyatlar = new DevExpress.XtraEditors.GroupControl();
+            this.Btn_Tesdiqle = new DevExpress.XtraEditors.SimpleButton();
+            this.btn_cisix_et = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.txtR_Proses.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateTarix.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateTarix.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Group_Emeliyyatlar)).BeginInit();
+            this.Group_Emeliyyatlar.SuspendLayout();
             this.SuspendLayout();
             // 
             // Lbn_Basliq
@@ -44,21 +57,137 @@
             this.Lbn_Basliq.Dock = System.Windows.Forms.DockStyle.Top;
             this.Lbn_Basliq.Location = new System.Drawing.Point(0, 0);
             this.Lbn_Basliq.Name = "Lbn_Basliq";
-            this.Lbn_Basliq.Size = new System.Drawing.Size(762, 43);
+            this.Lbn_Basliq.Size = new System.Drawing.Size(640, 43);
             this.Lbn_Basliq.TabIndex = 9;
-            this.Lbn_Basliq.Text = "Masa Melumati";
+            this.Lbn_Basliq.Text = "Masa Rezervasiya Et";
+            // 
+            // labelControl8
+            // 
+            this.labelControl8.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelControl8.Appearance.BackColor = System.Drawing.Color.White;
+            this.labelControl8.Appearance.BorderColor = System.Drawing.Color.Gray;
+            this.labelControl8.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl8.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.labelControl8.Appearance.Options.UseBackColor = true;
+            this.labelControl8.Appearance.Options.UseBorderColor = true;
+            this.labelControl8.Appearance.Options.UseFont = true;
+            this.labelControl8.Appearance.Options.UseForeColor = true;
+            this.labelControl8.Appearance.Options.UseTextOptions = true;
+            this.labelControl8.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
+            this.labelControl8.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl8.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.labelControl8.Location = new System.Drawing.Point(12, 52);
+            this.labelControl8.Name = "labelControl8";
+            this.labelControl8.Size = new System.Drawing.Size(133, 23);
+            this.labelControl8.TabIndex = 29;
+            this.labelControl8.Text = "Tarix :  ";
+            // 
+            // txtR_Proses
+            // 
+            this.txtR_Proses.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtR_Proses.Location = new System.Drawing.Point(154, 81);
+            this.txtR_Proses.Name = "txtR_Proses";
+            this.txtR_Proses.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtR_Proses.Properties.Appearance.Options.UseFont = true;
+            this.txtR_Proses.Size = new System.Drawing.Size(482, 104);
+            this.txtR_Proses.TabIndex = 33;
+            // 
+            // labelControl10
+            // 
+            this.labelControl10.Appearance.BackColor = System.Drawing.Color.White;
+            this.labelControl10.Appearance.BorderColor = System.Drawing.Color.Gray;
+            this.labelControl10.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl10.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.labelControl10.Appearance.Options.UseBackColor = true;
+            this.labelControl10.Appearance.Options.UseBorderColor = true;
+            this.labelControl10.Appearance.Options.UseFont = true;
+            this.labelControl10.Appearance.Options.UseForeColor = true;
+            this.labelControl10.Appearance.Options.UseTextOptions = true;
+            this.labelControl10.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
+            this.labelControl10.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl10.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.labelControl10.Location = new System.Drawing.Point(12, 81);
+            this.labelControl10.Name = "labelControl10";
+            this.labelControl10.Size = new System.Drawing.Size(133, 104);
+            this.labelControl10.TabIndex = 32;
+            this.labelControl10.Text = "Proses :  ";
+            // 
+            // dateTarix
+            // 
+            this.dateTarix.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dateTarix.EditValue = null;
+            this.dateTarix.Location = new System.Drawing.Point(156, 49);
+            this.dateTarix.Name = "dateTarix";
+            this.dateTarix.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.dateTarix.Properties.Appearance.Options.UseFont = true;
+            this.dateTarix.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dateTarix.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dateTarix.Properties.MaskSettings.Set("mask", "F");
+            this.dateTarix.Size = new System.Drawing.Size(478, 26);
+            this.dateTarix.TabIndex = 34;
+            // 
+            // Group_Emeliyyatlar
+            // 
+            this.Group_Emeliyyatlar.Controls.Add(this.Btn_Tesdiqle);
+            this.Group_Emeliyyatlar.Controls.Add(this.btn_cisix_et);
+            this.Group_Emeliyyatlar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Group_Emeliyyatlar.Location = new System.Drawing.Point(0, 193);
+            this.Group_Emeliyyatlar.Name = "Group_Emeliyyatlar";
+            this.Group_Emeliyyatlar.Size = new System.Drawing.Size(640, 95);
+            this.Group_Emeliyyatlar.TabIndex = 35;
+            this.Group_Emeliyyatlar.Text = "Əməliyyatlar";
+            // 
+            // Btn_Tesdiqle
+            // 
+            this.Btn_Tesdiqle.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Tesdiqle.Appearance.Options.UseFont = true;
+            this.Btn_Tesdiqle.Appearance.Options.UseTextOptions = true;
+            this.Btn_Tesdiqle.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.Btn_Tesdiqle.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Yeni_Sifaris.ImageOptions.Image")));
+            this.Btn_Tesdiqle.Location = new System.Drawing.Point(12, 26);
+            this.Btn_Tesdiqle.Name = "Btn_Tesdiqle";
+            this.Btn_Tesdiqle.Size = new System.Drawing.Size(133, 64);
+            this.Btn_Tesdiqle.TabIndex = 0;
+            this.Btn_Tesdiqle.Text = "Tesdiqle";
+            // 
+            // btn_cisix_et
+            // 
+            this.btn_cisix_et.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_cisix_et.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cisix_et.Appearance.Options.UseFont = true;
+            this.btn_cisix_et.Appearance.Options.UseTextOptions = true;
+            this.btn_cisix_et.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.btn_cisix_et.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_cisix_et.ImageOptions.SvgImage")));
+            this.btn_cisix_et.Location = new System.Drawing.Point(501, 26);
+            this.btn_cisix_et.Name = "btn_cisix_et";
+            this.btn_cisix_et.Size = new System.Drawing.Size(133, 64);
+            this.btn_cisix_et.TabIndex = 3;
+            this.btn_cisix_et.Text = "Çıxış";
             // 
             // Frm_Masa_Rezervasiya
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(762, 577);
+            this.ClientSize = new System.Drawing.Size(640, 288);
+            this.Controls.Add(this.Group_Emeliyyatlar);
+            this.Controls.Add(this.dateTarix);
+            this.Controls.Add(this.txtR_Proses);
+            this.Controls.Add(this.labelControl10);
+            this.Controls.Add(this.labelControl8);
             this.Controls.Add(this.Lbn_Basliq);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Frm_Masa_Rezervasiya";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Rezervasiya";
+            this.Text = "Masa Rezervasiya Et";
+            ((System.ComponentModel.ISupportInitialize)(this.txtR_Proses.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateTarix.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateTarix.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Group_Emeliyyatlar)).EndInit();
+            this.Group_Emeliyyatlar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -66,5 +195,12 @@
         #endregion
 
         private DevExpress.XtraEditors.LabelControl Lbn_Basliq;
+        private DevExpress.XtraEditors.LabelControl labelControl8;
+        private DevExpress.XtraEditors.MemoEdit txtR_Proses;
+        private DevExpress.XtraEditors.LabelControl labelControl10;
+        private DevExpress.XtraEditors.DateEdit dateTarix;
+        private DevExpress.XtraEditors.GroupControl Group_Emeliyyatlar;
+        private DevExpress.XtraEditors.SimpleButton Btn_Tesdiqle;
+        private DevExpress.XtraEditors.SimpleButton btn_cisix_et;
     }
 }
