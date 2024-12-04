1b080090881f7aa9e3584cfde0bcb25d707bfc0f
MuradoffTehmez <muradofftehmez01@gmail.com>
2024-12-04 14:02:02 +0400

    feat: Məhsul siyahısını göstərmək üçün Frm_UrunSec forması əlavə edildi.
    - Yeni forma `Frm_UrunSec` yaradıldı və DevExpress GridControl istifadə edilərək məhsul siyahısı göstərildi.
    - `CafeContext` və `UrunDAL` vasitəsilə verilənlər bazasından bütün məhsulların məlumatları çəkildi.
    - Məqsəd: İstifadəçilərə mövcud məhsulları sadə və intuitiv şəkildə təqdim etmək.
    - Tətbiq arxitekturası ilə uyğun olaraq `CafeRestoranApp.Entities.DAL` və `CafeRestoranApp.Entities.Models` siniflərindən istifadə edildi.
    - Gələcəkdə: İstifadəçinin məhsul seçimi ilə bağlı əməliyyatların əlavə edilməsi planlaşdırılır.

--- CofeRestoranApp.WinForms/Urunler/Frm_UrunSec.cs
+++ CofeRestoranApp.WinForms/Urunler/Frm_UrunSec.cs
@@ -1,14 +1,4 @@
-﻿using DevExpress.XtraEditors;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using CafeRestoranApp.Entities.DAL;
+﻿using CafeRestoranApp.Entities.DAL;
 using CafeRestoranApp.Entities.Models;
 
 namespace CofeRestoranApp.WinForms.Urunler
