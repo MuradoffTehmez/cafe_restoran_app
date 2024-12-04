95a0a7bb003030469b98d4aeb6c55f16822fc9f2
MuradoffTehmez <muradofftehmez01@gmail.com>
2024-12-04 14:00:51 +0400

    feat: Məhsul seçimi üçün yeni pəncərə (Frm_UrunSec) əlavə edildi.
    - Məhsulların siyahısını göstərən `Frm_UrunSec` forması yaradıldı.
    - DevExpress GridControl istifadə edilərək, `urunDal.GetAll(context)` metodu ilə məhsullar verilənlər bazasından çəkildi və siyahıya yükləndi.
    - Kodun strukturu "CafeRestoranApp" layihəsinin arxitekturasına uyğun yazıldı.
    - Məqsəd: İstifadəçilərin məhsulları vizual interfeys vasitəsilə seçməsini təmin etmək.
    - Gələcəkdə: Seçim sonrası əməliyyatların əlavə edilməsi planlaşdırılır.

--- CofeRestoranApp.WinForms/Urunler/Frm_UrunSec.cs
+++ CofeRestoranApp.WinForms/Urunler/Frm_UrunSec.cs
@@ -8,14 +8,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CafeRestoranApp.Entities.DAL;
+using CafeRestoranApp.Entities.Models;
 
 namespace CofeRestoranApp.WinForms.Urunler
 {
     public partial class Frm_UrunSec : DevExpress.XtraEditors.XtraForm
     {
+        private CafeContext context = new CafeContext();
+        private UrunDAL urunDal = new UrunDAL();
         public Frm_UrunSec()
         {
             InitializeComponent();
+            gridControl1.DataSource = urunDal.GetAll(context);
         }
     }
 }
