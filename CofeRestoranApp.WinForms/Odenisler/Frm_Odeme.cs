0a06500f60fb00c28802431ce95d191b4aa3141f
Təhməz Muradoff <muradofftehmez01@gmail.com>
2024-12-08 06:23:46 +0400

    Odenis ucun form yaratildi

---
+++ CofeRestoranApp.WinForms/Odenisler/Frm_Odeme.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CofeRestoranApp.WinForms.Odenisler
+{
+    public partial class Frm_Odeme : DevExpress.XtraEditors.XtraForm
+    {
+        public Frm_Odeme()
+        {
+            InitializeComponent();
+        }
+    }
+}
