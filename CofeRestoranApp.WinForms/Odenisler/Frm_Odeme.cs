81de96ebd5c85c36b6ba9a383f0026732f9f3281
Təhməz Muradoff <muradofftehmez01@gmail.com>
2024-12-10 18:58:45 +0400

    kod təmizliyi edildi

--- CofeRestoranApp.WinForms/Odenisler/Frm_Odeme.cs
+++ CofeRestoranApp.WinForms/Odenisler/Frm_Odeme.cs
@@ -1,15 +1,4 @@
-﻿using DevExpress.XtraEditors;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CofeRestoranApp.WinForms.Odenisler
+﻿namespace CofeRestoranApp.WinForms.Odenisler
 {
     public partial class Frm_Odeme : DevExpress.XtraEditors.XtraForm
     {
