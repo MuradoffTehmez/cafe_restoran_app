4db33480562c42f066d4d0c05bfde480f33f1647
Təhməz Muradoff <muradofftehmez01@gmail.com>
2024-12-11 04:48:17 +0400

    MasaGetir() metodunda verilənlər bazası əməliyyatlarında baş verə biləcək xətaları idarə etmək üçün əlavə edilib. DurumYenile() metodunda düymələrə tətbiq edilən dəyişikliklərin səhv işləməsinin qarşısını almaq üçün əlavə edilib. Btn_Click(), Btn_Yeni_Sifaris_Click(), Btn_Masa_Ac_Click(), və Btn_Rezev_Et_Click() metodlarında mümkün xətaların idarə olunması üçün əlavə edilib.

--- CofeRestoranApp.WinForms/Masalar/Frm_Masa_Durumu.cs
+++ CofeRestoranApp.WinForms/Masalar/Frm_Masa_Durumu.cs
@@ -23,118 +23,156 @@ namespace CofeRestoranApp.WinForms.Masalar
             InitializeComponent();
             MasaGetir();
         }
+
         private void Frm_Masa_Durumu_Load(object sender, EventArgs e)
         {
 
         }
+
         public void MasaGetir()
         {
-            flowLayoutPanel1.Controls.Clear();
-            var model = context.Masalar.ToList();
-            context = new CafeContext();
-            for (int i = 0; i < model.Count; i++)
+            try
             {
-                var btn = new CheckButton();
-                btn.Text = model[i].MasaAdi;
-                btn.Name = model[i].Id.ToString();
-                btn.Height = 150;
-                btn.Width = 125;
-                btn.Tag = model[i].SatisKodu;
-                flowLayoutPanel1.Controls.Add(btn);
-                if (model[i].Rezervasiya && !model[i].Durumu)
+                flowLayoutPanel1.Controls.Clear();
+                var model = context.Masalar.ToList();
+                context = new CafeContext();
+                for (int i = 0; i < model.Count; i++)
                 {
-                    btn.Appearance.BackColor = Color.Yellow;
+                    var btn = new CheckButton();
+                    btn.Text = model[i].MasaAdi;
+                    btn.Name = model[i].Id.ToString();
+                    btn.Height = 150;
+                    btn.Width = 125;
+                    btn.Tag = model[i].SatisKodu;
+                    flowLayoutPanel1.Controls.Add(btn);
+                    if (model[i].Rezervasiya && !model[i].Durumu)
+                    {
+                        btn.Appearance.BackColor = Color.Yellow;
+                    }
+                    else if (model[i].Durumu)
+                    {
+                        btn.Appearance.BackColor = Color.Red;
+                    }
+                    else if (!model[i].Durumu)
+                    {
+                        btn.Appearance.BackColor = Color.Green;
+                    }
+                    btn.Click -= Btn_Click;
+                    btn.Click += Btn_Click;
                 }
-                else if (model[i].Durumu)
-                {
-                    btn.Appearance.BackColor = Color.Red;
-                }
-                else if (!model[i].Durumu)
-                {
-                    btn.Appearance.BackColor = Color.Green;
-                }
-                btn.Click -= Btn_Click;
-                btn.Click += Btn_Click;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Xəta baş verdi: " + ex.Message, "Xəta", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-
         public void DurumYenile()
         {
-            Btn_Yeni_Sifaris.Enabled = false;
-            Btn_Masa_Ac.Enabled = false;
-            Btn_Rezev_Et.Enabled = false;
-
+            try
+            {
+                Btn_Yeni_Sifaris.Enabled = false;
+                Btn_Masa_Ac.Enabled = false;
+                Btn_Rezev_Et.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Xəta baş verdi: " + ex.Message, "Xəta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void Btn_Click(object sender, EventArgs e)
         {
-            BtnSender = sender as CheckButton;
-            _masaId = Convert.ToInt32(BtnSender.Name);
-            _SatisKodu = BtnSender.Tag?.ToString();
-            DurumYenile();
-            if (BtnSender.Appearance.BackColor == Color.Yellow)
+            try
             {
-                Btn_Masa_Ac.Enabled = true;
+                BtnSender = sender as CheckButton;
+                _masaId = Convert.ToInt32(BtnSender.Name);
+                _SatisKodu = BtnSender.Tag?.ToString();
+                DurumYenile();
+                if (BtnSender.Appearance.BackColor == Color.Yellow)
+                {
+                    Btn_Masa_Ac.Enabled = true;
+                }
+                else if (BtnSender.Appearance.BackColor == Color.Green)
+                {
+                    Btn_Masa_Ac.Enabled = true;
+                    Btn_Rezev_Et.Enabled = true;
+                }
+                else if (BtnSender.Appearance.BackColor == Color.Red)
+                {
+                    Btn_Yeni_Sifaris.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show(
+                        "Xəta: Bu hadisə CheckButton növündə bir obyekt ilə tetiklenmedi.",
+                        "Xəta",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
             }
-            else if (BtnSender.Appearance.BackColor == Color.Green)
+            catch (Exception ex)
             {
-                Btn_Masa_Ac.Enabled = true;
-                Btn_Rezev_Et.Enabled = true;
-            }
-            else if (BtnSender.Appearance.BackColor == Color.Red)
-            {
-                Btn_Yeni_Sifaris.Enabled = true;
-            }
-            else
-            {
-                MessageBox.Show(
-                    "Xəta: Bu hadisə CheckButton növündə bir obyekt ilə tetiklenmedi.",
-                    "Xəta",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Error
-                );
+                MessageBox.Show("Xəta baş verdi: " + ex.Message, "Xəta", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void Btn_Yeni_Sifaris_Click(object sender, EventArgs e)
         {
-            //_SatisKodu = BtnSender.Tag.ToString();
-            Frm_Masa_Siparis frm = new Frm_Masa_Siparis(masaId: _masaId, masaadi: BtnSender.Text, satiskodu: _SatisKodu);
-            frm.ShowDialog();
-            BtnSender = null;
-            DurumYenile();
-            MasaGetir();
+            try
+            {
+                Frm_Masa_Siparis frm = new Frm_Masa_Siparis(masaId: _masaId, masaadi: BtnSender.Text, satiskodu: _SatisKodu);
+                frm.ShowDialog();
+                BtnSender = null;
+                DurumYenile();
+                MasaGetir();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Xəta baş verdi: " + ex.Message, "Xəta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void Btn_Masa_Ac_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show(BtnSender.Text + "  Acilsin ?", "Melumat", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            try
             {
-                masalar = masalarDal.GetByFilter(context, m => m.Id == _masaId);
-                
-                masalar.SatisKodu = modelSatisKodu.SatisTanimi + modelSatisKodu.Sayi;
-                masalar.Durumu = true;
-                modelSatisKodu.Sayi++;
-                masalarDal.Save(context);
-                BtnSender = null;
-                DurumYenile();
-                MasaGetir();
+                if (MessageBox.Show(BtnSender.Text + "  Acilsin ?", "Melumat", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+                    masalar = masalarDal.GetByFilter(context, m => m.Id == _masaId);
+                    masalar.SatisKodu = modelSatisKodu.SatisTanimi + modelSatisKodu.Sayi;
+                    masalar.Durumu = true;
+                    modelSatisKodu.Sayi++;
+                    masalarDal.Save(context);
+                    BtnSender = null;
+                    DurumYenile();
+                    MasaGetir();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Xəta baş verdi: " + ex.Message, "Xəta", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void Btn_Rezev_Et_Click(object sender, EventArgs e)
         {
-            Frm_Masa_Rezervasiya frm = new Frm_Masa_Rezervasiya(_masaId);
-            frm.ShowDialog();
-            DurumYenile();
-            if (frm.islemyapildi)
+            try
             {
-                //DurumYenile();
-                MasaGetir();
-                //BtnSender = null;
+                Frm_Masa_Rezervasiya frm = new Frm_Masa_Rezervasiya(_masaId);
+                frm.ShowDialog();
+                DurumYenile();
+                if (frm.islemyapildi)
+                {
+                    MasaGetir();
+                }
+                BtnSender = null;
             }
-            BtnSender = null;
+            catch (Exception ex)
+            {
+                MessageBox.Show("Xəta baş verdi: " + ex.Message, "Xəta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
